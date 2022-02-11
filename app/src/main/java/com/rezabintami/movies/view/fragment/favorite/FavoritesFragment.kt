c0529ff5515a7969ed3f78845f72fb847e06ca86
rezabintami <rezabintami@gmail.com>
2022-02-11 23:57:54 +0700

    Create Detail Movies Activity

--- app/src/main/java/com/rezabintami/movies/view/fragment/favorite/FavoritesFragment.kt
+++ app/src/main/java/com/rezabintami/movies/view/fragment/favorite/FavoritesFragment.kt
@@ -1,10 +1,12 @@
 package com.rezabintami.movies.view.fragment.favorite
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.rezabintami.movies.R
 import com.rezabintami.movies.databinding.FragmentFavoritesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,5 +24,21 @@ class FavoritesFragment: Fragment() {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initViewModelObserver()
+    }
+
+    private fun initView(){
+        binding.header.textViewHeader.text = getString(R.string.favorites)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun initViewModelObserver() {
+
     }
 }
--- app/src/main/java/com/rezabintami/movies/view/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/rezabintami/movies/view/fragment/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.rezabintami.movies.view.fragment.home
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.rezabintami.common.data.Resource
+import com.rezabintami.common.ui.MoviesAdapter
 import com.rezabintami.movies.R
 import com.rezabintami.movies.databinding.FragmentHomeBinding
+import com.rezabintami.movies.view.activity.DetailMoviesActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,12 +21,15 @@ class HomeFragment: Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = requireNotNull(_binding) { "Binding view must not be null" }
     private val viewModel: HomeViewModel by activityViewModels()
+    private var adapterMoviesNowPlaying: MoviesAdapter? = null
+    private var adapterMoviesPopular: MoviesAdapter? = null
+    private var adapterMoviesTopRated: MoviesAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         return binding.root
@@ -38,10 +44,24 @@ class HomeFragment: Fragment() {
 
     private fun initView(){
         binding.header.textViewHeader.text = getString(R.string.home)
+        adapterMoviesNowPlaying = MoviesAdapter()
+        adapterMoviesPopular = MoviesAdapter()
+        adapterMoviesTopRated = MoviesAdapter()
+
+        binding.rvNowPlaying.adapter = adapterMoviesNowPlaying
+        binding.rvPopular.adapter = adapterMoviesPopular
+        binding.rvTopRated.adapter = adapterMoviesTopRated
+
+        adapterMoviesNowPlaying?.onItemClick = { selectedData ->
+            val intent = Intent(activity, DetailMoviesActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     @SuppressLint("SetTextI18n")
     private fun initViewModelObserver(){
+
+        // Now Playing
         viewModel.moviesNowPlaying.observe(viewLifecycleOwner, { moviesNowPlaying ->
             if(moviesNowPlaying != null) {
                 when(moviesNowPlaying){
@@ -49,16 +69,53 @@ class HomeFragment: Fragment() {
                     is Resource.Success -> {
                         binding.progressBarNowPlaying.visibility = View.GONE
                     // Add Adapter
+                        adapterMoviesNowPlaying?.setData(moviesNowPlaying.data)
                     }
                     is Resource.Error -> {
                         binding.progressBarNowPlaying.visibility = View.GONE
                         binding.viewErrorNowPlaying.root.visibility = View.VISIBLE
                         binding.viewErrorNowPlaying.tvError.text = moviesNowPlaying.message ?: getString(R.string.something_wrong)
-
                     }
                 }
             }
+        })
 
+        // Popular
+        viewModel.moviesNowPlaying.observe(viewLifecycleOwner, { moviesNowPlaying ->
+            if(moviesNowPlaying != null) {
+                when(moviesNowPlaying){
+                    is Resource.Loading -> binding.progressBarNowPlaying.visibility = View.VISIBLE
+                    is Resource.Success -> {
+                        binding.progressBarNowPlaying.visibility = View.GONE
+                        // Add Adapter
+                        adapterMoviesPopular?.setData(moviesNowPlaying.data)
+                    }
+                    is Resource.Error -> {
+                        binding.progressBarNowPlaying.visibility = View.GONE
+                        binding.viewErrorNowPlaying.root.visibility = View.VISIBLE
+                        binding.viewErrorNowPlaying.tvError.text = moviesNowPlaying.message ?: getString(R.string.something_wrong)
+                    }
+                }
+            }
+        })
+
+        //Top Rated
+        viewModel.moviesNowPlaying.observe(viewLifecycleOwner, { moviesNowPlaying ->
+            if(moviesNowPlaying != null) {
+                when(moviesNowPlaying){
+                    is Resource.Loading -> binding.progressBarNowPlaying.visibility = View.VISIBLE
+                    is Resource.Success -> {
+                        binding.progressBarNowPlaying.visibility = View.GONE
+                        // Add Adapter
+                        adapterMoviesTopRated?.setData(moviesNowPlaying.data)
+                    }
+                    is Resource.Error -> {
+                        binding.progressBarNowPlaying.visibility = View.GONE
+                        binding.viewErrorNowPlaying.root.visibility = View.VISIBLE
+                        binding.viewErrorNowPlaying.tvError.text = moviesNowPlaying.message ?: getString(R.string.something_wrong)
+                    }
+                }
+            }
         })
     }
 }
